ef41aec5a9e18e59eb0b3a216eb6edbbb8b9c3c3
Paul Kim <pk960805@gmail.com>
2016-12-13 11:59:33 -0800

    Create project

---
+++ README.md
@@ -0,0 +1,1 @@
+Testing android db using content provider
---
+++ app/src/main/java/com/paul/paulk/testdatabase/provider/ContentProvider.java
@@ -0,0 +1,8 @@
+package com.paul.paulk.testdatabase.provider;
+
+/**
+ * Created by paulk on 12/13/2016.
+ */
+
+public class ContentProvider {
+}
